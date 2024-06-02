c36fec17b41d0cfc6c19c5517c7e05b2466f9286
Francy Santos <francy.horray@gmail.com>
2024-06-02 02:56:10 -0300

    :construction: feat: added initial structure & express

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Staging!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello mimanga");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
