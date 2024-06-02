3aef99dcd584b2c21e25b5ae53c3fb66d7e7a4af
Francy Santos <francy.horray@gmail.com>
2024-06-02 02:45:18 -0300

    :sparkles: feat: initial setup w/ tailwind & shadcn-ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="text-3xl underline text-red-500">Mimanga</h1>
       <Button>Test</Button>
     </>
   );
