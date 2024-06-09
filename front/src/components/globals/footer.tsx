f5ae7d98ad29c0a9060e4daef1f5947b4b89506d
Francy Santos <francy.horray@gmail.com>
2024-06-08 22:17:17 -0300

    :fire: separate front & back into different projects

--- front/src/components/globals/footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="w-full bg-slate-900 h-20 text-white flex items-center justify-center">
-      All rights deserved.
-    </footer>
-  );
-};
